e0123dc45800ad4d623956a028a60c8f28ca1f57
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-06-15 08:27:05 -0400

    trying to update packages

---
+++ server/src/types/AuthPayload.ts
@@ -0,0 +1,10 @@
+
+// import { objectType } from '@nexus/schema'
+
+// export const AuthPayload = objectType({
+//   name: 'AuthPayload',
+//   definition(t) {
+//     t.string('token')
+//     t.field('user', { type: 'User' })
+//   },
+// })
